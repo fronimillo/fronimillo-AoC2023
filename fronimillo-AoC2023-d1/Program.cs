d8529adeb9679c446c45457441e3dbe928a9bb5e
Camillo Hess <hess.camillo@fronius.com>
2025-03-24 16:35:35 +0100

    deleted old code and added/now use IndexofAny

--- fronimillo-AoC2023-d1/Program.cs
+++ fronimillo-AoC2023-d1/Program.cs
@@ -2,12 +2,7 @@
 using System.Collections.Generic;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-public class Program
-{
-    public const string input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
-    List<char> numbers = new() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-    public static void Main()
-    {
+
 /*
  * read out every position of string
  * for every char do:
@@ -24,25 +19,23 @@ public class Program
  *      this is called codesum and is undefined chars long
  * add all codenumber only when all strings have been "decoded"
 */
-
-        int i = 0;
-        foreach (char code in input)
+public class Program
+{
+    // fix later
+    public static char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    public static string input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+    public static void Main()
+    {
+        var splitinput = input.Split('\n');
+        var resultList = new List<int>();
+        foreach (var line in splitinput)
         {
-            if (Char.IsNumber(code))
-            {
-                //foreach (char twocode in code)
-                //foreach (char codes in input)
-                i++;
-                Console.Write($"{code}");
-                if (i >= 2)
-                {
-                    //Console.WriteLine($"{code}");
-                    Console.Write(" ");
-                    i = 0;
-                }
-
-            }
-            //i = 0;
+            int digitFirst = line.IndexOfAny(digits);
+            int digitLast = line.LastIndexOfAny(digits);
+            var lineResult = $"{digitFirst}{digitLast}";
+            var numberResult = int.Parse(lineResult);
+            resultList.Add(numberResult);
+            Console.WriteLine(numberResult);
         }
     }
-}  
+}
