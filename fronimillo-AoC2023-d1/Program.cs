0c6792870375ee8a40cba716d58996b9f3c46b98
Camillo Hess <hess.camillo@fronius.com>
2025-03-25 12:29:48 +0100

    added File input functionality - outputs correct value 1st Puzzle solved

--- fronimillo-AoC2023-d1/Program.cs
+++ fronimillo-AoC2023-d1/Program.cs
@@ -7,13 +7,14 @@ public class Program
 {
     public static char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
     public static string input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+    public static string[] fileInput = File.ReadAllLines(@"C:\Users\hess.camillo\Documents\puzzleinput.txt");
     public static void Main()
     {
         int sumNumber = 0;
         var splitinput = input.Split('\n');
         var resultList = new List<int>();
         
-        foreach (var line in splitinput)
+        foreach (var line in fileInput)
         {
             int digitFirst = line.IndexOfAny(digits);
             int digitLast = line.LastIndexOfAny(digits);
@@ -26,5 +27,3 @@ public class Program
         Console.WriteLine(sumNumber);
     }
 }
-
-// puzzle input dir.: C:\Users\hess.camillo\Documents\puzzleinput.txt
