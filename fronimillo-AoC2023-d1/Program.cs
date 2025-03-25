0e2e5a8ad9378d89257d6f45bcb941ca846cf360
Camillo Hess <hess.camillo@fronius.com>
2025-03-25 17:16:41 +0100

    replaced old, unused input with alphabeticDigits for furture use.

--- fronimillo-AoC2023-d1/Program.cs
+++ fronimillo-AoC2023-d1/Program.cs
@@ -6,12 +6,12 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 public class Program
 {
     public static char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-    public static string input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+    public static string alphabeticDigits = "one\ntwo\nthree\nfour\nfive\nsix\nseven\neight\nnine";
     public static string[] fileInput = File.ReadAllLines(@"C:\Users\hess.camillo\Documents\puzzleinput.txt");
     public static void Main()
     {
         int sumNumber = 0;
-        var splitinput = input.Split('\n');
+        var splitinput = alphabeticDigits.Split('\n');
         var resultList = new List<int>();
         
         foreach (var line in fileInput)
