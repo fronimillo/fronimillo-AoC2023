067fe9e035b1a8b82556baba021469963a055ce9
Camillo Hess <hess.camillo@fronius.com>
2025-03-25 17:54:04 +0100

    renamed digits to numDigits and reworked alphabeticDigits to string array

--- fronimillo-AoC2023-d1/Program.cs
+++ fronimillo-AoC2023-d1/Program.cs
@@ -5,24 +5,29 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Program
 {
-    public static char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-    public static string alphabeticDigits = "one\ntwo\nthree\nfour\nfive\nsix\nseven\neight\nnine";
+    public static char[] numDigits = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    public static string[] alphaDigits = { "one", "two", "nthree", "nfour", "five", "six", "nseven", "eight", "nine" };
     public static string[] fileInput = File.ReadAllLines(@"C:\Users\hess.camillo\Documents\puzzleinput.txt");
     public static void Main()
     {
+        // Part 1 of Day 1
         int sumNumber = 0;
-        var splitinput = alphabeticDigits.Split('\n');
         var resultList = new List<int>();
-        
         foreach (var line in fileInput)
         {
-            int digitFirst = line.IndexOfAny(digits);
-            int digitLast = line.LastIndexOfAny(digits);
+            int digitFirst = line.IndexOfAny(numDigits);
+            int digitLast = line.LastIndexOfAny(numDigits);
             var lineResult = $"{line[digitFirst]}{line[digitLast]}";
             var numberResult = int.Parse(lineResult);
             resultList.Add(numberResult);
             sumNumber = sumNumber + numberResult;
         }
         Console.WriteLine(sumNumber);
+        //////////////////////////////////////////////////////////////////////////////
+        // Part 2 of Day 1
+        foreach ( var line in fileInput)
+        {
+            
+        }
     }
 }
