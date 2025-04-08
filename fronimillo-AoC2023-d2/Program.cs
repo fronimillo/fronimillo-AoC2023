bf2c0ee1a0808c2b57409df07c9094e4800cf8f1
Camillo Hess <hess.camillo@fronius.com>
2025-04-08 15:12:55 +0200

    added puzzleExample string array

--- fronimillo-AoC2023-d2/Program.cs
+++ fronimillo-AoC2023-d2/Program.cs
@@ -2,8 +2,9 @@
 public class Program
 {
     public static string[] fileInput = File.ReadAllLines(@"C:\Users\hess.camillo\Documents\puzzleinputday2.txt");
+    public static string[] puzzleExample = { "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\r\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\r\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\r\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\r\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" }
     public static void Main()
     {
-
+        // only 12 red cubes, 13 green cubes, and 14 blue cubes
     }
 }
