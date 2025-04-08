4beaa3d9f30111a784a39272cfd9e3478349fc5f
Camillo Hess <hess.camillo@fronius.com>
2025-04-08 16:19:19 +0200

    fromated line 5

--- fronimillo-AoC2023-d2/Program.cs
+++ fronimillo-AoC2023-d2/Program.cs
@@ -2,14 +2,14 @@
 public class Program
 {
     public static string[] fileInput = File.ReadAllLines(@"C:\Users\hess.camillo\Documents\puzzleinputday2.txt");
-    public static string[] puzzleExample = { "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" +
-                                         "\r\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue" +
-                                         "\r\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" +
-                                         "\r\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red" +
-                                         "\r\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" };
+    public static string[] puzzleExample = { "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                                             "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                                             "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                                             "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                                             "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" };
     public static void Main()
     {
-        /*
+        /* WHAT DO I  H*A*V*E  TO DO?!
         // Determine in the first place if Games is even possible with from each set amount?
         // => Filter If any Set already contains to many Cubes for possible game? => Dif ***
 
@@ -29,7 +29,7 @@ public class Program
         foreach (var game in puzzleExample)
         { 
             Console.WriteLine(game);
-
+            
         }
     }
 }
