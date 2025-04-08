ae64433b18dd3f91884f84d6309f125234a5070b
Camillo Hess <hess.camillo@fronius.com>
2025-04-08 16:07:57 +0200

    added mission comments

--- fronimillo-AoC2023-d2/Program.cs
+++ fronimillo-AoC2023-d2/Program.cs
@@ -10,10 +10,23 @@ public class Program
     public static void Main()
     {
         // only 12 red cubes, 13 green cubes, and 14 blue cubes
-        // 1,2 & 5 are possible games in puzzleExample => Why?
+        // 1,2 & 5 are possible games in puzzleExample => Why? => because the amount of showed cubes is less than the constraints in line above.
+        // each SET is seperated by an semicolon (;) => just for fun prediction for part 2 of this day => diferantion between Sets is new Task
+        // game 1 3rd set is just 2 green cubes
+        // Explantion:
+        // game 1 == 9 blue, 5 red, 4 green => 9blue <= 14; 5red <= 12; 4green <= 13 ==> possible game == True
         foreach (var game in puzzleExample)
         { 
             Console.WriteLine(game);
+            // Determine in the first place if Games is even possible with from each set amount?
+            // => Filter If any Set already contains to many Cubes for possible game? => Dif ***
+
+            // Addition of all filterd Amounts of Cubes and sort by Color.
+            // => Dif. *
+
+            // Determine in the second place if Game is even possible for each color cube amount that has been added up?
+            // => Find Possible Games via Addition of ceratin Color Cube amount  => Dif ***
+
         }
     }
 }
