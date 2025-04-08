34c0834b4ed8f4083119018a1457e96b26280a9e
Camillo Hess <hess.camillo@fronius.com>
2025-04-08 15:04:42 +0200

    added puzzle input string array and Main loop

--- fronimillo-AoC2023-d2/Program.cs
+++ fronimillo-AoC2023-d2/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
-
 public class Program
 {
+    public static string[] fileInput = File.ReadAllLines(@"C:\Users\hess.camillo\Documents\puzzleinputday2.txt");
+    public static void Main()
+    {
 
+    }
 }
